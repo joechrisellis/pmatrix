6b91d7b34002e71c6dd4fb499226a7380c192cae
Joe Ellis <jce.devel@gmail.com>
2014-04-21 21:06:10 +0100

    Minor changes.

--- pmatrix/pmatrix.py
+++ pmatrix/pmatrix.py
@@ -4,7 +4,6 @@ import random
 from string import printable
 from time import sleep
 
-COLOR = curses.COLOR_GREEN
 COLORS = {
 	"BLUE" : curses.COLOR_BLUE,
 	"CYAN" : curses.COLOR_CYAN,
@@ -15,25 +14,20 @@ COLORS = {
 	"YELLOW" : curses.COLOR_YELLOW,
 }
 
-LETTERS_PER_UPDATE = 2
-UPDATE_DELAY = 0.03
 rand_string = lambda c, l: "".join(random.choice(c) for i in xrange(l))
 
 def main(stdscr):
 	curses.curs_set(0)
 	curses.init_pair(9, COLOR, curses.COLOR_BLACK)
 	curses.start_color()
-	initial_size = stdscr.getmaxyx()
+	size = stdscr.getmaxyx()
 
-	back = [rand_string(printable.strip(), initial_size[1]) for i in xrange(initial_size[0])]
+	back = rand_string(printable.strip(), size[0] * size[1])
 	dispense, visible = [], []
 	
 	while 1:
-		size = stdscr.getmaxyx()
-		if size != initial_size:
+		if stdscr.getmaxyx() != size:
 			return # returns back to start()
-
-		stdscr.clear()
 
 		for i in xrange(LETTERS_PER_UPDATE):
 			dispense.append(random.randint(0, size[1] - 1))
@@ -43,23 +37,24 @@ def main(stdscr):
 			if not random.randint(0, 5):
 				del dispense[i]
 
-		for i, c in enumerate(visible):
-			if c[0] < size[0] - 1:
-				stdscr.addstr(c[0], c[1], back[c[0]][c[1]], curses.color_pair(9))
-				c[0] += 1
+		for a, b in enumerate(visible):
+			if b[0] < size[0] - 1:
+				stdscr.addstr(b[0], b[1], back[b[0] * size[0] + b[1]], curses.color_pair(9))
+				b[0] += 1
 			else:
 				# can't use del obj since it's not a reference to the
 				# list, but instead a copy. we have to delete by index.
-				del visible[i]
+				del visible[a]
 
 		stdscr.refresh()
 		sleep(UPDATE_DELAY)
+		stdscr.clear()
 
 def start():
 	parser = optparse.OptionParser()
 	parser.add_option("-c", "--color", default="green",
 			help="The colour of the falling text.")
-	parser.add_option("-d", "--delay", type=float, default=0.03,
+	parser.add_option("-d", "--delay", type=float, default=0.05,
 			help="The update delay.")
 	parser.add_option("-l", "--letters", type=int, default=2,
 			help="The number of letters produced per update.")
