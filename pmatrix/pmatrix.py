3c8c9c1b3579ff6b518dbbe92fdcf98810ea9ad1
Joe Ellis <jce.devel@gmail.com>
2014-04-22 18:37:52 +0100

    Added delta to regulate updates per second (experimental).

--- pmatrix/pmatrix.py
+++ pmatrix/pmatrix.py
@@ -1,8 +1,8 @@
+from string import printable
 import curses
 import optparse
 import random
-from string import printable
-from time import sleep
+import time
 
 COLORS = {
 	"BLUE" : curses.COLOR_BLUE,
@@ -24,46 +24,61 @@ def main(stdscr):
 
 	back = rand_string(printable.strip(), size[0] * size[1])
 	dispense, visible = [], []
-	
+
+	lt = time.time()
+	delta = 0
 	while 1:
-		if stdscr.getmaxyx() != size:
-			return # returns back to start()
+		now = time.time()
+		delta += (now - lt) * UPDATES_PER_SECOND
+		lt = now
 
-		for i in xrange(LETTERS_PER_UPDATE):
-			dispense.append(random.randint(0, size[1] - 1))
+		while delta >= 1:
 
-		for i, c in enumerate(dispense):
-			visible.append([0, c])
-			if not random.randint(0, 5):
-				del dispense[i]
+			if stdscr.getmaxyx() != size:
+				return # returns back to start()
 
-		for a, b in enumerate(visible):
-			if b[0] < size[0] - 1:
-				stdscr.addstr(b[0], b[1], back[b[0] * size[0] + b[1]], curses.color_pair(9))
-				b[0] += 1
-			else:
-				# can't use del obj since it's not a reference to the
-				# list, but instead a copy. we have to delete by index.
-				del visible[a]
+			for i in xrange(LETTERS_PER_UPDATE):
+				dispense.append(random.randint(0, size[1] - 1))
 
-		stdscr.refresh()
-		sleep(UPDATE_DELAY)
-		stdscr.clear()
+			for i, c in enumerate(dispense):
+				visible.append([0, c])
+				if not random.randint(0, 5):
+					del dispense[i]
+
+			for a, b in enumerate(visible):
+				if b[0] < size[0] - 1:
+					stdscr.addstr(b[0], b[1], back[b[0] * size[0] + b[1]], curses.color_pair(9))
+					b[0] += 1
+				else:
+					# can't use del obj since it's not a reference to the
+					# list, but instead a copy. we have to delete by index.
+					del visible[a]
+
+			delta -= 1
+			stdscr.refresh()
+
+		if ERASE:
+			stdscr.erase()
+		else:
+			stdscr.clear()
 
 def start():
 	parser = optparse.OptionParser()
 	parser.add_option("-c", "--color", default="green",
 			help="The colour of the falling text.")
-	parser.add_option("-d", "--delay", type=float, default=0.05,
-			help="The update delay.")
+	parser.add_option("-e", "--erase", action="store_true",
+			help="Use stdscr.erase(), which may reduce flicker.")
 	parser.add_option("-l", "--letters", type=int, default=2,
 			help="The number of letters produced per update.")
+	parser.add_option("-u", "--ups", type=int, default=30,
+			help="The number of updates to perform per second.")
 	options, args = parser.parse_args()
 
-	global COLOR, LETTERS_PER_UPDATE, UPDATE_DELAY
+	global COLOR, ERASE, LETTERS_PER_UPDATE, UPDATES_PER_SECOND
 	COLOR = COLORS.get(options.color.upper(), curses.COLOR_GREEN)
+	ERASE = options.erase
 	LETTERS_PER_UPDATE = abs(options.letters)
-	UPDATE_DELAY = abs(options.delay)
+	UPDATES_PER_SECOND = abs(options.ups)
 
 	try:
 		while 1: curses.wrapper(main)
