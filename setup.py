9d4b9ca9a2babbcfc7ac974dc33c1c9b628a0599
Joseph Ellis <jce.devel@gmail.com>
2015-02-22 15:31:46 +0000

    Changed email.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import *
 
 kwargs = {
     "author" : "Joe Ellis",
-    "author_email" : "jce.devel@gmail.com",
+    "author_email" : "joechrisellis@gmail.com",
     "description" : "A Python-implementation of the matrix falling text (like cmatrix).",
     "entry_points" : {"console_scripts" : ["pmatrix=pmatrix.pmatrix:start"]},
     "license" : "GPL v2",
